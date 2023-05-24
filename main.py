af652f90fc0bfd1208cf95dd1e4c2703d6566235
Stella <stellaxxxwall@gmail.com>
2023-05-24 17:12:07 +0800

    initialize project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def hello(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'hello, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    hello('World')
+
+
