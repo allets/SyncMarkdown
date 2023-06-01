2123b5b28d82ec1e4113ac0ce36bdd21d82cef0b
Stella <stellaxxxwall@gmail.com>
2023-06-01 12:43:19 +0800

    generate 2 csv files, `index-image.csv` and `index-image-tmp.csv`

--- sync_md.py
+++ sync_md.py
@@ -3,7 +3,9 @@ import csv
 import datetime
 import logging
 import os
+import random
 import re
+import shutil
 import sys
 
 from enum import IntEnum, unique
@@ -22,7 +24,7 @@ logging.basicConfig(level=logging.DEBUG,
                     format=LOGGING_FORMAT)
 
 MD_INDEX_FIELD_NAMES = ["FileName", "MdUrl", "ModifiedDate", "IsSynced"]
-IMG_INDEX_FIELD_NAMES = ["FileName", "IsDownloaded", "ImageUrl", "ImageName"]
+IMG_INDEX_FIELD_NAMES = ["MdFileName", "IsDownloaded", "ImageUrl", "ImageName"]
 FIELD_MODIFIED_DATE_FORMAT = "%Y/%m/%d %H:%M:%S.%f %z"
 
 
@@ -53,6 +55,22 @@ def md_index_raw_record_to_md_index_record(raw) -> MdIndexRecord:
                            raw["MdUrl"],
                            MdIndexIsSynced(int(raw["IsSynced"])),
                            datetime.datetime.strptime(raw["ModifiedDate"], FIELD_MODIFIED_DATE_FORMAT))
+    return record
+
+
+class ImgIndexRecord(DataPrintable):
+    def __init__(self, md_filename, is_downloaded: bool, img_url, img_name):
+        self.md_filename = md_filename
+        self.is_downloaded = is_downloaded
+        self.img_url = img_url
+        self.img_name = img_name
+
+
+def img_index_raw_record_to_img_index_record(raw) -> ImgIndexRecord:
+    record = ImgIndexRecord(raw["MdFileName"],
+                            bool(int(raw["IsDownloaded"])),
+                            raw["ImageUrl"],
+                            raw["ImageName"])
     return record
 
 
@@ -161,6 +179,11 @@ class MdIndexReader:
 
         return record
 
+    def list_record(self):
+        for row in self._get_reader():
+            record = md_index_raw_record_to_md_index_record(row)
+            yield record
+
 
 class ImgIndexWriter:
 
@@ -182,6 +205,88 @@ class ImgIndexWriter:
             logging.error(f"\nException type: {e_type}"
                           f"\nException value: {e_value}"
                           f"\nTraceback: {traceback}\n")
+
+    def create(self, record: ImgIndexRecord):
+        self._writer.writerow(
+            {"MdFileName": record.md_filename,
+             "IsDownloaded": int(record.is_downloaded),
+             "ImageUrl": record.img_url,
+             "ImageName": record.img_name})
+
+    def create_by_raw_records(self, raw_records):
+        for r in raw_records:
+            record = {}
+            for field_name in IMG_INDEX_FIELD_NAMES:
+                record[field_name] = r.get(field_name)
+
+            self._writer.writerow(record)
+
+
+class ImgIndexReader:
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def __enter__(self):
+        logging.debug(f"open: {self.filepath}")
+        self._file = open(self.filepath, newline="", encoding="utf-8")
+        self._reader = csv.DictReader(self._file, quoting=csv.QUOTE_ALL)
+        self._record_index_by_md_filename = self._index_records_by_md_filename()
+        return self
+
+    def __exit__(self, e_type, e_value, traceback):
+        if not e_type:
+            logging.debug(f"close: {self.filepath}")
+            self._file.close()
+        else:
+            logging.error(f"\nException type: {e_type}"
+                          f"\nException value: {e_value}"
+                          f"\nTraceback: {traceback}\n")
+
+    def _get_reader(self):
+        # logging.debug(f"before {self._reader.line_num}, {self._reader.reader.line_num}")
+        if self._reader.line_num > 0:
+            self._file.seek(0)
+            next(self._reader)  # skip header
+            # logging.debug(f"after {self._reader.line_num}, {self._reader.reader.line_num}")
+        return self._reader
+
+    def _index_records_by_md_filename(self):
+        record_index = {}
+        for idx, row in enumerate(self._get_reader()):
+            record = img_index_raw_record_to_img_index_record(row)
+            index_list = record_index.get(record.md_filename)
+            if index_list is None:
+                record_index[record.md_filename] = index_list = []
+
+            index_list.append(idx)
+
+        return record_index
+
+    def get_raw_records_by_md_filename(self, md_filename):
+        records = []
+        index_list = self._record_index_by_md_filename.get(md_filename)
+        if not index_list:
+            return records
+
+        for idx, row in enumerate(self._get_reader()):
+            if idx in index_list:
+                records.append(row)
+
+        return records
+
+    def get_records_by_md_filename(self, md_filename):
+        records = []
+        index_list = self._record_index_by_md_filename.get(md_filename)
+        if not index_list:
+            return records
+
+        for idx, row in enumerate(self._get_reader()):
+            if idx in index_list:
+                record = img_index_raw_record_to_img_index_record(row)
+                records.append(record)
+
+        return records
 
 
 def merge_md_filenames(md_dir_path, old_md_index_path):
@@ -235,7 +340,7 @@ def generate_md_index(md_dir_path, md_url_index_path, old_md_index_path, md_inde
             MdIndexWriter(tmp_md_index_path) as tmp_md_index:
 
         for md_filename in md_filenames:
-            md_path = f"{md_dir_path}/{md_filename}"
+            md_path = f"{md_dir_path}/{md_filename}"  # !!! md_path may not be existed.
             md_url = md_url_mapping.get(md_filename)
 
             if os.path.exists(md_path):
@@ -254,7 +359,14 @@ def generate_md_index(md_dir_path, md_url_index_path, old_md_index_path, md_inde
                         tmp_md_index.create(record)
 
                     else:
-                        md_index.create(record)
+                        if record.is_synced != MdIndexIsSynced.Y:
+                            record.is_synced = MdIndexIsSynced.N
+
+                            md_index.create(record)
+                            tmp_md_index.create(record)
+
+                        else:
+                            md_index.create(record)
 
                 else:
                     record = MdIndexRecord(md_filename, md_url, MdIndexIsSynced.N_FIRST, modified_date)
@@ -284,6 +396,141 @@ def mock_old_index():
         pass
 
     return [old_md_index_path, old_img_index_path]
+
+
+def copy_md_files(md_input_dir_path, md_output_dir_path):
+    logging.debug(f"\n=== copy_md_files ====================================\n"
+                  f"from {md_input_dir_path}\n"
+                  f"to {md_output_dir_path}\n"
+                  f"=======================================================\n")
+
+    shutil.rmtree(md_output_dir_path)
+    shutil.copytree(md_input_dir_path, md_output_dir_path, dirs_exist_ok=True)
+
+
+def parse_img_urls_in_md(md_path, acceptance_predicate):
+    img_urls = set()
+
+    if not os.path.exists(md_path) or os.path.isdir(md_path):
+        return img_urls
+
+    with open(md_path, newline="", encoding="utf-8") as md:
+        for row in md:
+            line = row.strip()
+
+            # markdown img ex: `![img alt](https://i.imgur.com/bbb.png)`
+            # https://regexr.com/7el72
+            pattern = r"\!\[(.*)\]\((https*:\/\/([^\)]+))\)"
+            result = re.search(pattern, line)
+            if result is not None:
+                groups = result.groups()
+                # logging.debug(f"img_url match groups= {groups}")
+                alt, link, link_without_protocol = groups
+                if acceptance_predicate is None \
+                        or acceptance_predicate(link):
+                    img_urls.add(link)
+                else:
+                    logging.info(f"excluding img_url\n  {link}")
+
+    return img_urls
+
+
+def is_acceptable_img_url(img_url):
+    acceptance = (
+        "https://i.imgur.com/",
+        "https://i.stack.imgur.com/",
+    )
+
+    if img_url.startswith(acceptance, 0, len(img_url) - 1):
+        return True
+    else:
+        return False
+
+
+def generate_img_name(img_url):
+    idx = img_url.rfind("/")
+    url_page = img_url[idx + 1:]
+    norm_name = os.path.normcase(url_page)
+    # logging.debug(f"generate_img_name from `{img_url}`\n  {url_page}\n  {norm_name}")
+
+    num = random.randint(1, 100)  # avoid duplicate name
+
+    img_name = f"{num}-{norm_name}"
+
+    return img_name
+
+
+def generate_img_index(md_dir_path, md_index_path,
+                       old_img_index_path, img_index_path, tmp_img_index_path, delete_img_list_path):
+    with MdIndexReader(md_index_path) as md_index, \
+            ImgIndexReader(old_img_index_path) as old_img_index, \
+            ImgIndexWriter(img_index_path) as img_index, \
+            ImgIndexWriter(tmp_img_index_path) as tmp_img_index, \
+            open(delete_img_list_path, mode="w", newline="", encoding="utf-8") as delete_img_list:
+        md_records = md_index.list_record()
+        for md_record in md_records:
+            md_path = f"{md_dir_path}/{md_record.filename}"  # !!! md_path may not be existed.
+
+            if not os.path.exists(md_path) \
+                    or md_record.is_synced == MdIndexIsSynced.Y:
+                old_records = old_img_index.get_raw_records_by_md_filename(md_record.filename)
+                img_index.create_by_raw_records(old_records)
+
+            elif md_record.is_synced == MdIndexIsSynced.N_FIRST:
+                img_urls = parse_img_urls_in_md(md_path, is_acceptable_img_url)
+                # if img_urls:
+                #     logging.debug(f"image urls in `{md_record.filename}`\n  {img_urls}")
+
+                for img_url in img_urls:
+                    img_name = generate_img_name(img_url)
+                    record = ImgIndexRecord(md_record.filename, False, img_url, img_name)
+
+                    img_index.create(record)
+                    tmp_img_index.create(record)
+
+            elif md_record.is_synced == MdIndexIsSynced.N:
+                img_urls = parse_img_urls_in_md(md_path, is_acceptable_img_url)
+                # if img_urls:
+                #     logging.debug(f"image urls in `{md_record.filename}`\n  {img_urls}")
+
+                old_records = old_img_index.get_records_by_md_filename(md_record.filename)
+
+                old_img_urls = set()
+                old_record_index = {}
+                for r in old_records:
+                    old_img_urls.add(r.img_url)
+                    old_record_index[r.img_url] = r
+
+                duplicate_img_urls = img_urls & old_img_urls
+                new_img_urls = img_urls - old_img_urls
+                deleted_img_urls = old_img_urls - img_urls
+
+                for img_url in duplicate_img_urls:
+                    record = old_record_index[img_url]
+
+                    img_index.create(record)
+                    if not record.is_downloaded:
+                        tmp_img_index.create(record)
+
+                for img_url in new_img_urls:
+                    img_name = generate_img_name(img_url)
+                    record = ImgIndexRecord(md_record.filename, False, img_url, img_name)
+
+                    img_index.create(record)
+                    tmp_img_index.create(record)
+
+                for img_url in deleted_img_urls:
+                    record = old_record_index[img_url]
+
+                    (md_filename_without_ext, sep, ext) = record.md_filename.rpartition(".")
+                    if sep == "." and ext == "md":
+                        img_dir_name = md_filename_without_ext
+                    else:
+                        img_dir_name = record.md_filename
+
+                    img_path = f"{img_dir_name}/{record.img_name}"
+
+                    delete_img_list.write(f"{img_path}\n")
 
 
 def sync_md(md_dir_path, md_url_index_path, old_md_index_path, old_img_index_path):
@@ -316,6 +563,15 @@ def sync_md(md_dir_path, md_url_index_path, old_md_index_path, old_img_index_pat
     md_index_path = f"{output_dir}/index-markdown.csv"
     tmp_md_index_path = f"{output_dir}/index-markdown-tmp.csv"
     generate_md_index(md_dir_path, md_url_index_path, old_md_index_path, md_index_path, tmp_md_index_path)
+
+    md_output_dir_path = f"{output_dir}/SyncedMd"
+    copy_md_files(md_dir_path, md_output_dir_path)
+
+    img_index_path = f"{output_dir}/index-image.csv"
+    tmp_img_index_path = f"{output_dir}/index-image-tmp.csv"
+    delete_img_list_path = f"{output_dir}/deleteImgList.txt"
+    generate_img_index(md_output_dir_path, md_index_path,
+                       old_img_index_path, img_index_path, tmp_img_index_path, delete_img_list_path)
 
 
 def main():
